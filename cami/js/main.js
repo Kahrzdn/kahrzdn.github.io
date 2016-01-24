4bb22668b8bc54ccb6a4682c051a73cb1f571654
Carsten Jørgensen <to.bogstaver@gmail.com>
2016-01-24 22:48:17 +0100

    collision and scrolling

--- cami/js/main.js
+++ cami/js/main.js
@@ -24,7 +24,8 @@ navigator.mediaDevices.msGetUserMedia);
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'phaser', {
     preload: preload,
     create: create,
-    render: render
+    render: render,
+    update: update
 });
 
 var emitter;
@@ -33,44 +34,81 @@ var videoTexture;
 var map;
 var layer;
 var posspr;
+var npc;
+var npcs = [];
+var delta = 4;
 
 function preload() {
 
     game.load.image('corona', 'media/star.png');
     game.load.image('tiles', 'media/tileset.png');
     game.load.tilemap('tilemap', 'media/tileset.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet('sheet', 'media/tileset.png', 23, 23, 30 * 20);
 }
 
 function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
     game.stage.backgroundColor = '#000000';
 
     videoFrame = game.add.sprite(0, 0);
 
 
-    map = game.add.tilemap('tilemap')
-    game.world.setBounds(0, 0, 20000, 2000);
-    posspr = game.add.sprite(450, 80, 'corona');
+    map = game.add.tilemap('tilemap');
+    game.world.setBounds(0, 0, 10000, 2000);
+
+    posspr = game.add.sprite(450, 30, 'sheet');
+    posspr.frame = 179;
     posspr.anchor.setTo(0.5, 0.5);
+    //game.physics.enable(posspr);
+    //posspr.body.gravity.y = 1000;
+    //game.physics.arcade.collide(posspr, map);
     game.camera.follow(posspr);
     map.addTilesetImage('tiles-world1', 'tiles');
-    layer = map.create('level1', 600, 30, 21, 21);
-    layer.scrollFactorX = 0.5;
-    layer.scrollFactorY = 0.5;
-    layer.wrap =false;
+    map.setCollision([123, 126, 127, 152, 156, 157, 179, 200]);
+    layer = map.create('level1', 600, 40, 21, 21);
+    //   layer.scrollFactorX = 0.5;
+    //   layer.scrollFactorY = 0.5;
+    layer.wrap = false;
+    layer.debug = false;
+
     videoFrame.fixedToCamera = true;
     //layer.scroll = 8;
-    for (var i=0;i<900;i++)
-        map.putTile(121+i%1,i%map.layer.width,19);
+    var height = 10;
+    var lx = 0;
+    for (var i = 0; i < 900; i++) {
+        var r = Math.random();
+        for (var j = 1; j <= height; j++) {
+            map.putTile(152, lx, 40 - height + j);
+        }
+        if (r > 0.9 && height < 20) {
+            height++;
+            map.putTile(126, lx, 40 - height);
+            map.putTile(156, lx, 40 - height + 1);
 
+        }
+        else if (r < 0.1 && height > 3) {
+            height--;
+            map.putTile(200, lx, 40 - height - 2);
+            map.putTile(127, lx, 40 - height - 1);
+            map.putTile(157, lx, 40 - height);
+        }
+        else {
+            map.putTile(123, lx, 40 - height);
+        }
+        lx++;
+    }
     emitter = game.add.emitter(game.world.centerX, game.world.centerY, 200);
 
     emitter.fixedToCamera = true;
-    emitter.makeParticles('corona');
+    var particules = 6;
+    var _pArray = [10, 12, 20];
+
+    emitter.makeParticles('sheet', [376, 377, 378, 379, 179]);
 
     emitter.setRotation(0, 0);
-    emitter.setAlpha(0.3, 0.8);
-    emitter.setScale(0, .2, 0, .2);
+    emitter.setAlpha(0.7, 1);
+    emitter.setScale(1, 1, 1, 1);
     emitter.gravity = 1000;
 
     //	false means don't explode all the sprites at once, but instead release at a rate of one particle per 100ms
@@ -79,13 +117,61 @@ function create() {
 
 }
 
+function createNewNPC() {
+    if (Math.random() < 0.009) {
+        npc = game.add.sprite(950 + game.camera.x, 420, 'sheet');
+        npc.frame = 26;
+        npc.anchor.setTo(0.5, 0.5);
+        game.physics.enable(npc);
+        npc.body.collideWorldBounds = true;
+        npc.body.bounce.y = 0.2;
+        npc.body.gravity.y = 1000;
+
+        //npcs.push(npc);
+    }
+}
+
 function render() {
 
-    update();
+    createNewNPC();
+    drawVideo();
+    blend();
+    showCenter();
+    drawTiles();
+
+}
+
+function update() {
+   // game.physics.arcade.collide(npc, layer);
+    if (npc && !npc.dead) {
+        game.physics.arcade.collide(emitter, npc, hit, null, this);
+        game.physics.arcade.collide(npc, layer, change, null, this);
+    }
+    game.physics.arcade.collide(emitter, layer, change, null, this);
+
+}
+
+function change(a, b) {
+   // console.log("dsd");
+}
+
+function hit(a, b) {
+    a.frame +=30;
+    console.log(a);
+    console.log(b);
+    console.log("------");
+
 }
 
 function drawTiles() {
-    posspr.x+=4;
+    posspr.x += delta;
+    if (posspr.x >= game.world.bounds.x+game.world.bounds.width) {
+        delta = -Math.abs(delta);
+    }
+    if (posspr.x <= game.world.bounds.x) {
+        delta = Math.abs(delta);
+    }
+
     //layer.position.x=(layer.position.x-1)%21;
     //for (var i=0;i<900;i++) {
     //    map.putTile(209,i%map.layer.width,~~(i/map.layer.width));
@@ -165,13 +251,6 @@ window.requestAnimFrame = (function () {
         };
 })();
 
-function update() {
-    drawVideo();
-    blend();
-    showCenter();
-    drawTiles();
-}
-
 
 function drawVideo() {
     contextSource.drawImage(video, 0, 0, canvasSource.width, canvasSource.height);//video.width, video.height);
@@ -210,8 +289,8 @@ function threshold(value) {
 function showCenter() {
     var c = computeCenter(contextBlended.getImageData(0, 0, canvasSource.width, canvasSource.height));
     if (c.x && c.y) {
-        emitter.x = c.x+videoFrame.x;
-        emitter.y = c.y+videoFrame.y;
+        emitter.x = videoFrame.x + c.x;
+        emitter.y = videoFrame.y + c.y;
         // console.log(c.x);
     }
 }

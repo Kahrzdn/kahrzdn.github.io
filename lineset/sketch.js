d895f83f3f98ad875550daa30e6c14bc1b68d9ea
Carsten Jørgensen <to.bogstaver@gmail.com>
2023-08-01 11:36:11 +0200

    Add files via upload

--- lineset/sketch.js
+++ lineset/sketch.js
@@ -1,16 +1,31 @@
-const colorMap = [{r:50,g:80,b:80},{r:140,g:150,b:140},{r:220,g:240,b:200}]
+const colorMap = [
+  "#ff0000",
+  "#ffa500",
+  "#ffff00",
+  "#008000",
+  "#0000ff",
+  "#4b0082",
+  "#ee82ee"]
 
-var levels = [{lanes:
-  [[0,1,2,0,0,2,0,2],
-  [1,2,0,0,2,1,2,0],
-  [2,0,0,2,1,2,0,1]]},
-  {lanes:
-    [[0,1,2,0,0,2,1,2],
-    [0,0,0,0,0,2,1,2],
-    [0,1,2,0,0,2,1,2]]},
+var levels = [{
+  lanes:
+    [[0, 1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 4, 5, 6],
+    [0, 1, 2, 3, 4, 5, 6]]
+},
+{
+  lanes:
+    [[0, 1, 2, 0, 0, 2, 1, 2],
+    [0, 0, 0, 0, 0, 2, 1, 2],
+    [0, 1, 2, 0, 0, 2, 1, 2]]
+},
 ]
 
-var currentLevel=0;
+var currentLevel = 0;
 var wdx;
 var wdy;
 
@@ -19,37 +34,29 @@ function setup() {
 }
 
 function drawLevel(level) {
-  wdx=windowWidth/level.lanes.length;
-  wdy=windowHeight/level.lanes[0].length;
-  if (lanePos==0)
-     drawLane(0,laneDY,level.lanes[0])
-  else 
-     drawLane(0,0,level.lanes[0]);
-
-  if (lanePos==1)
-     drawLane(1,laneDY,level.lanes[1]);
-  else 
-     drawLane(1,0,level.lanes[1]);
-
-  if (lanePos==2)
-     drawLane(2,laneDY,level.lanes[2]);
-  else 
-     drawLane(2,0,level.lanes[2]);
-
-}
-
-function drawLane(place,dy,lane) {
-  for(var i=0;i<lane.length;i++){
-    drawBrick(place,i,dy,lane[i]);
+  wdx = windowWidth / level.lanes.length;
+  wdy = windowHeight / level.lanes[0].length;
+  for (var i = 0; i < level.lanes.length; i++) {
+    if (lanePos == i)
+      drawLane(i, laneDY, level.lanes[i])
+    else
+      drawLane(i, 0, level.lanes[i]);
   }
 }
 
-function drawBrick(px,py,dy,colNum){
-  col=colorMap[colNum];
-  fill(col.r,col.g,col.b)
-  rect(px*wdx,py*wdy+dy,wdx,wdy);
+function drawLane(place, dy, lane) {
+  for (var i = 0; i < lane.length; i++) {
+    drawBrick(place, i, dy, lane[i]);
+  }
 }
-  
+
+function drawBrick(px, py, dy, colNum) {
+  col = colorMap[colNum];
+  noStroke();
+  fill(col);
+  rect(px * wdx, py * wdy + dy, wdx, wdy);
+}
+
 function draw() {
   background(0);
   drawLevel(levels[currentLevel])
@@ -60,43 +67,51 @@ function windowResized() {
 
 }
 
-var mousepos = {x:-1,y:-1}
+var mousepos = { x: -1, y: -1 }
 function arrayRotate(arr, reverse) {
   if (reverse) arr.unshift(arr.pop());
   else arr.push(arr.shift());
   return arr;
 }
 
-var laneDY=0;
-var lanePos =0;
+var laneDY = 0;
+var lanePos = 0;
 
 function touchStarted() {
-  mousepos.x=mouseX;
-  mousepos.y=mouseY; 
+  mousepos.x = mouseX;
+  mousepos.y = mouseY;
 }
 
-function touchMoved () {
-  laneDY = mouseY-mousepos.y;
-  lanePos = floor(mousepos.x/wdx);
+function touchMoved() {
+  laneDY = mouseY - mousepos.y;
+  lanePos = floor(mousepos.x / wdx);
 }
 
 function touchEnded() {
-  const deltaY = round((mouseY-mousepos.y)/wdy);
+  const deltaY = round((mouseY - mousepos.y) / wdy);
 
-  console.log("dssd"+lanePos)
-  if (deltaY<0) {
-    for(var i = 0;i<-deltaY;i++) {
-      arrayRotate(levels[currentLevel].lanes[lanePos],false);  
+  console.log("dssd" + lanePos)
+  if (deltaY < 0) {
+    for (var i = 0; i < -deltaY; i++) {
+      arrayRotate(levels[currentLevel].lanes[lanePos], false);
     }
   }
-  if (deltaY>0) {
-    for(var i = 0;i<deltaY;i++) {
-      arrayRotate(levels[currentLevel].lanes[lanePos],true);  
+  if (deltaY > 0) {
+    for (var i = 0; i < deltaY; i++) {
+      arrayRotate(levels[currentLevel].lanes[lanePos], true);
     }
   }
-  laneDY=0;
-} 
+  laneDY = 0;
+}
 
-document.ontouchmove = function(event) {
+function touchHandler(event) {
+  if (event.touches.length > 1) {
+    //the event is multi-touch
+    //you can then prevent the behavior
+    event.preventDefault()
+  }
+}
+
+document.ontouchmove = function (event) {
   event.preventDefault();
 };
